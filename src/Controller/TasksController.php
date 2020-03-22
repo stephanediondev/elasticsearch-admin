9e75f120214b92892a4426fc747b0913694e0790
Stéphane Dion <divers@sdion.net>
2020-03-22 20:34:27 +0100

    -

---
+++ src/Controller/TasksController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AbstractAppController;
+use App\Form\AliasType;
+use App\Form\IndiceType;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TasksController extends AbstractAppController
+{
+    /**
+     * @Route("tasks", name="tasks")
+     */
+    public function index(Request $request): Response
+    {
+        $query = [
+            'group_by' => 'none',
+        ];
+        $tasks = $this->queryManager->query('GET', '/_tasks', ['query' => $query]);
+        $tasks = $tasks['tasks'];
+
+        return $this->renderAbstract($request, 'tasks_index.html.twig', [
+            'tasks' => $this->paginatorManager->paginate([
+                'route' => 'tasks',
+                'route_parameters' => [],
+                'total' => count($tasks),
+                'rows' => $tasks,
+                'page' => 1,
+                'size' => count($tasks),
+            ]),
+        ]);
+    }
+}
