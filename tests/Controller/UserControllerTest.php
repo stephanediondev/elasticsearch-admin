d30612a25d8860617e1b053d77f87da9f71460dc
Stéphane Dion <divers@sdion.net>
2020-06-26 14:17:01 +0200

    -

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -73,6 +73,54 @@ class UserControllerTest extends AbstractAppControllerTest
     }
 
     /**
+     * @Route("/users/{user}/disable", name="users_disable")
+     */
+    public function testDisable404()
+    {
+        $this->client->request('GET', '/admin/users/'.uniqid().'/disable');
+
+        $this->assertResponseStatusCodeSame(404);
+    }
+
+    public function testDisable403()
+    {
+        $this->client->request('GET', '/admin/users/elastic/disable');
+
+        $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function testDisable()
+    {
+        $this->client->request('GET', '/admin/users/elasticsearch-admin-test/disable');
+
+        $this->assertResponseStatusCodeSame(302);
+    }
+
+    /**
+     * @Route("/users/{user}/enable", name="users_enable")
+     */
+    public function testEnable404()
+    {
+        $this->client->request('GET', '/admin/users/'.uniqid().'/enable');
+
+        $this->assertResponseStatusCodeSame(404);
+    }
+
+    public function testEnable403()
+    {
+        $this->client->request('GET', '/admin/users/elastic/enable');
+
+        $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function testEnable()
+    {
+        $this->client->request('GET', '/admin/users/elasticsearch-admin-test/enable');
+
+        $this->assertResponseStatusCodeSame(302);
+    }
+
+    /**
      * @Route("/users/{user}/delete", name="users_delete")
      */
     public function testDelete404()
