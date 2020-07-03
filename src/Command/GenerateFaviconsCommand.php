61dae290810543e39149cc2ab783d6bb626153fc
Stéphane Dion <divers@sdion.net>
2020-07-03 09:22:26 +0200

    -

---
+++ src/Command/GenerateFaviconsCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Command;
+
+use App\Manager\CallManager;
+use App\Model\CallRequestModel;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenerateFaviconsCommand extends Command
+{
+    protected static $defaultName = 'app:generate-favicons';
+
+    public function __construct(CallManager $callManager)
+    {
+        $this->callManager = $callManager;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Generate favicons');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        foreach ($this->colors as $name => $code) {
+            $this->generateFavicon($name, $this->size);
+        }
+
+        return Command::SUCCESS;
+    }
+
+    private $size = 64;
+
+    private $colors = [
+        'red' => 'dc3545',
+        'yellow' => 'ffc107',
+        'green' => '28a745',
+    ];
+
+    private function generateFavicon($color, $size) {
+        $file = __DIR__.'/../../public/favicon-'.$color.'.png';
+
+        $image = imagecreate($size, $size);
+
+        $split = str_split($this->colors[$color], 2);
+        $r = hexdec($split[0]);
+        $g = hexdec($split[1]);
+        $b = hexdec($split[2]);
+
+        $color = imagecolorallocate($image, $r, $g, $b);
+
+        imagerectangle($image, 0, 0, $size, $size, $color);
+
+        imagepng($image, $file);
+    }
+}
---
+++ src/Controller/ScreenshotsController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AbstractAppController;
+use App\Model\CallRequestModel;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/admin")
+ */
+class ScreenshotsController extends AbstractAppController
+{
+    /**
+     * @Route("/screenshots", name="screenshots")
+     */
+    public function index(Request $request): Response
+    {
+        $cookie = session_id();
+
+        $base = $request->getSchemeAndHttpHost().$request->getBaseURL();
+
+        $screenshots = [
+            //'login' => '/',
+            'cluster' => '/admin/cluster',
+            'cluster-settings' => '/admin/cluster/settings',
+            'cluster-allocation-explain' => '/admin/cluster/allocation/explain',
+            'index-create' => '/admin/indices/create',
+            'index-templates' => '/admin/index-templates',
+            'index-template-create' => '/admin/index-templates/create',
+            'indices' => '/admin/indices',
+            'indices-stats' => '/admin/indices/stats',
+            'index' => '/admin/indices/elasticsearch-admin-test',
+            'nodes' => '/admin/nodes',
+            'node' => '/admin/nodes/macos',
+            'snapshots' => '/admin/snapshots',
+            'snapshot-create' => '/admin/snapshots/create',
+            'repository-create-s3' => '/admin/repositories/create/s3',
+            'slm-policy-create' => '/admin/slm/create',
+            'shards' => '/admin/shards',
+            'enrich-create' => '/admin/enrich/create',
+            //'license' => '/admin/license',
+        ];
+
+        $results = [];
+        foreach ($screenshots as $filename => $screenshot) {
+            $results[] = [
+                'pageres' => 'pageres '.$base.$screenshot.' 1280x768 --crop --filename=assets/images/original-'.$filename.' --overwrite --cookie=\'PHPSESSID='.$cookie.'\'',
+                'convert' => 'convert -resize 800x480 assets/images/original-'.$filename.'.png assets/images/resized-'.$filename.'.png',
+            ];
+        }
+
+        return $this->renderAbstract($request, 'Modules/screenshots/screenshots_index.html.twig', [
+            'results' => $results,
+        ]);
+    }
+}
