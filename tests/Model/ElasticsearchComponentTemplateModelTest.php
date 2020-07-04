24d2c14fadb1f37a60bf1d85714cd6cf978f1d97
Stéphane Dion <divers@sdion.net>
2020-07-04 21:51:50 +0200

    -

---
+++ tests/Model/ElasticsearchComponentTemplateModelTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Tests\Model;
+
+use App\Model\ElasticsearchComponentTemplateModel;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ElasticsearchComponentTemplateModelTest extends WebTestCase
+{
+    public function test()
+    {
+        $template = new ElasticsearchComponentTemplateModel();
+        $template->setName('name');
+        $template->setVersion(1);
+        $template->setSettings([]);
+        $template->setSetting('setting-key', 'setting-value');
+        $template->setMappings([]);
+        $template->setAliases([]);
+
+        $this->assertEquals($template->getName(), 'name');
+
+        $this->assertEquals($template->getVersion(), 1);
+        $this->assertIsInt($template->getVersion());
+
+        $this->assertEquals($template->getSettings(), ['setting-key' => 'setting-value']);
+        $this->assertIsArray($template->getSettings());
+        $this->assertEquals($template->getSetting('setting-key'), 'setting-value');
+
+        $this->assertEquals($template->getMappings(), []);
+        $this->assertIsArray($template->getMappings());
+
+        $this->assertEquals($template->getAliases(), []);
+        $this->assertIsArray($template->getAliases());
+
+        $this->assertEquals($template->isSystem(), false);
+
+        $template->setName('.name');
+        $this->assertEquals($template->isSystem(), true);
+    }
+}
