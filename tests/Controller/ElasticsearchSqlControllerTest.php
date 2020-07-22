b42ffc41240988f4c09697e579f556f1537f4c6c
Stéphane Dion <divers@sdion.net>
2020-07-22 22:21:26 +0200

    -

---
+++ tests/Controller/ElasticsearchSqlControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests\Controller;
+
+/**
+ * @Route("/admin")
+ */
+class ElasticsearchSqlControllerTest extends AbstractAppControllerTest
+{
+    /**
+     * @Route("/sql", name="sql")
+     */
+    public function testIndex()
+    {
+        $this->client->request('GET', '/admin/sql');
+
+        if (false == $this->callManager->hasFeature('sql')) {
+            $this->assertResponseStatusCodeSame(403);
+        } else {
+            $this->assertResponseStatusCodeSame(200);
+            $this->assertPageTitleSame('SQL REST API');
+        }
+    }
+}
