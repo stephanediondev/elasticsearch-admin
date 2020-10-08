dbf032f563e960ed551adc9eaef23eff095fb623
Stéphane Dion <divers@sdion.net>
2020-10-08 14:15:02 +0200

    -

--- update.sh
+++ update.sh
@@ -3,3 +3,6 @@ git reset --hard origin/main
 composer install
 npm install
 npm run build
+
+current_timestamp=`date +%Y%m%d.%H%M%S`
+sed -i "s/VERSION =.*/VERSION = '${current_timestamp}';/g" public/serviceworker.js
