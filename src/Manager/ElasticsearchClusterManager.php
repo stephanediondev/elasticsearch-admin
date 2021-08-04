e9a9185ba3a6a5d4bd14c371962947c55bcf9222
Stéphane Dion <divers@sdion.net>
2021-08-04 15:06:47 +0200

    -

--- src/Manager/ElasticsearchClusterManager.php
+++ src/Manager/ElasticsearchClusterManager.php
@@ -90,6 +90,7 @@ class ElasticsearchClusterManager extends AbstractAppManager
             ['es_version' => '7.11.0', 'eol_date' => '2022-08-10', 'maintained_until' => '7.12.0'],
             ['es_version' => '7.12.0', 'eol_date' => '2022-09-23', 'maintained_until' => '7.13.0'],
             ['es_version' => '7.13.0', 'eol_date' => '2022-11-25', 'maintained_until' => '7.14.0'],
+            ['es_version' => '7.14.0', 'eol_date' => '2023-02-03', 'maintained_until' => '7.15.0'],
         ];
     }
 
