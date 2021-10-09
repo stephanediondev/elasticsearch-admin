0cb4616fea5098a8d6e96939d71bde4675aab9c6
Stéphane Dion <divers@sdion.net>
2021-10-09 07:20:08 +0200

    define available permissions based on features

--- src/Manager/AppRoleManager.php
+++ src/Manager/AppRoleManager.php
@@ -13,99 +13,6 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class AppRoleManager extends AbstractAppManager
 {
-    private $attributes = [
-        'global' => [
-            'CLUSTER_SETTINGS', 'CLUSTER_SETTING_EDIT', 'CLUSTER_SETTING_REMOVE', 'CLUSTER_DISK_THRESHOLDS', 'CLUSTER_ALLOCATION_EXPLAIN', 'CLUSTER_AUDIT',
-            'NODES', 'NODES_STATS', 'NODES_RELOAD_SECURE_SETTINGS',
-            'INDICES', 'INDICES_STATS', 'INDICES_CREATE', 'INDICES_REINDEX', 'INDICES_FORCE_MERGE', 'INDICES_CACHE_CLEAR', 'INDICES_FLUSH', 'INDICES_REFRESH',
-            'DATA_STREAMS', 'DATA_STREAMS_CREATE',
-            'SHARDS', 'SHARDS_STATS', 'SHARDS_REROUTE',
-            'MENU_CONFIGURATION',
-            'INDEX_TEMPLATES_LEGACY', 'INDEX_TEMPLATES_LEGACY_CREATE',
-            'INDEX_TEMPLATES', 'INDEX_TEMPLATES_CREATE',
-            'COMPONENT_TEMPLATES', 'COMPONENT_TEMPLATES_CREATE',
-            'ILM_POLICIES', 'ILM_POLICIES_STATUS',
-            'ILM_POLICIES_CREATE',
-            'SLM_POLICIES', 'SLM_POLICIES_STATS', 'SLM_POLICIES_STATUS', 'SLM_POLICIES_CREATE',
-            'REPOSITORIES', 'REPOSITORIES_CREATE',
-            'ENRICH_POLICIES', 'ENRICH_POLICIES_STATS', 'ENRICH_POLICIES_CREATE',
-            'ELASTICSEARCH_USERS', 'ELASTICSEARCH_USERS_CREATE',
-            'ELASTICSEARCH_ROLES', 'ELASTICSEARCH_ROLES_CREATE',
-            'MENU_TOOLS',
-            'SNAPSHOTS', 'SNAPSHOTS_STATS', 'SNAPSHOTS_CREATE',
-            'PIPELINES', 'PIPELINES_CREATE',
-            'TASKS',
-            'REMOTE_CLUSTERS',
-            'CAT', 'CAT_EXPORT',
-            'SQL',
-            'INDEX_GRAVEYARD',
-            'DANGLING_INDICES', 'DANGLING_INDICES_IMPORT', 'DANGLING_INDICES_DELETE',
-            'CONSOLE', 'CONSOLE_POST', 'CONSOLE_PUT', 'CONSOLE_PATCH', 'CONSOLE_DELETE',
-            'DEPRECATIONS',
-            'LICENSE', 'LICENSE_START_TRIAL', 'LICENSE_START_BASIC',
-            'MENU_STATS',
-            'MENU_APPLICATION',
-            'APP_USERS', 'APP_USERS_CREATE',
-            'APP_ROLES', 'APP_ROLES_CREATE',
-            'APP_UNINSTALL', 'APP_UPGRADE', 'APP_SUBSCRIPTIONS', 'APP_NOTIFICATIONS',
-        ],
-        'app_user' => [
-            'APP_USER_UPDATE', 'APP_USER_DELETE',
-        ],
-        'app_role' => [
-            'APP_ROLE_UPDATE', 'APP_ROLE_DELETE',
-        ],
-        'component_template' => [
-            'COMPONENT_TEMPLATE_UPDATE', 'COMPONENT_TEMPLATE_DELETE', 'COMPONENT_TEMPLATE_COPY',
-        ],
-        'enrich_policy' => [
-            'ENRICH_POLICY_DELETE', 'ENRICH_POLICY_COPY', 'ENRICH_POLICY_EXECUTE',
-        ],
-        'ilm_policy' => [
-            'ILM_POLICY_UPDATE', 'ILM_POLICY_DELETE', 'ILM_POLICY_COPY', 'ILM_POLICY_APPLY',
-        ],
-        'index_template_legacy' => [
-            'INDEX_TEMPLATE_LEGACY_UPDATE', 'INDEX_TEMPLATE_LEGACY_DELETE', 'INDEX_TEMPLATE_LEGACY_COPY',
-        ],
-        'index_template' => [
-            'INDEX_TEMPLATE_UPDATE', 'INDEX_TEMPLATE_DELETE', 'INDEX_TEMPLATE_COPY',
-        ],
-        'index' => [
-            'INDEX_UPDATE',
-            'INDEX_DELETE',
-            'INDEX_CLOSE', 'INDEX_OPEN',
-            'INDEX_FREEZE', 'INDEX_UNFREEZE',
-            'INDEX_FORCE_MERGE', 'INDEX_CACHE_CLEAR', 'INDEX_FLUSH', 'INDEX_REFRESH', 'INDEX_EMPTY',
-            'INDEX_SEARCH', 'INDEX_IMPORT', 'INDEX_EXPORT',
-            'INDEX_LIFECYCLE',
-            'INDEX_ALIASES', 'INDEX_ALIAS_CREATE', 'INDEX_ALIAS_DELETE',
-        ],
-        'data_stream' => [
-            'DATA_STREAM_STATS', 'DATA_STREAM_DELETE',
-        ],
-        'node' => [
-            'NODE_SETTINGS', 'NODE_PLUGINS', 'NODE_USAGE',
-        ],
-        'pipeline' => [
-            'PIPELINE_UPDATE', 'PIPELINE_DELETE', 'PIPELINE_COPY',
-        ],
-        'repository' => [
-            'REPOSITORY_UPDATE', 'REPOSITORY_DELETE', 'REPOSITORY_CLEANUP', 'REPOSITORY_VERIFY',
-        ],
-        'slm_policy' => [
-            'SLM_POLICY_UPDATE', 'SLM_POLICY_DELETE', 'SLM_POLICY_COPY', 'SLM_POLICY_EXECUTE',
-        ],
-        'snapshot' => [
-            'SNAPSHOT_DELETE', 'SNAPSHOT_RESTORE', 'SNAPSHOT_CLONE', 'SNAPSHOT_FAILURES',
-        ],
-        'elasticsearch_user' => [
-            'ELASTICSEARCH_USER_UPDATE', 'ELASTICSEARCH_USER_DELETE', 'ELASTICSEARCH_USER_ENABLE', 'ELASTICSEARCH_USER_DISABLE',
-        ],
-        'elasticsearch_role' => [
-            'ELASTICSEARCH_ROLE_UPDATE', 'ELASTICSEARCH_ROLE_DELETE', 'ELASTICSEARCH_ROLE_COPY',
-        ],
-    ];
-
     private bool $permissionsDefined = false;
 
     private array $permissions = [];
@@ -315,18 +222,213 @@ class AppRoleManager extends AbstractAppManager
         return $roles;
     }
 
-    public function getAttributes()
+    public function getAttributes(): array
     {
-        ksort($this->attributes);
-        foreach ($this->attributes as $module => $permissions) {
+        $attributes = $this->getAttributesRaw();
+
+        ksort($attributes);
+        foreach ($attributes as $module => $permissions) {
             sort($permissions);
-            $this->attributes[$module] = $permissions;
+            $attributes[$module] = $permissions;
         }
-        return $this->attributes;
+        return $attributes;
     }
 
-    public function getAttributesByModule($module)
+    public function getAttributesByModule(string $module): array
     {
-        return $this->attributes[$module] ?? [];
+        $attributes = $this->getAttributesRaw();
+
+        return $attributes[$module] ?? [];
+    }
+
+    public function getAttributesRaw(): array
+    {
+        $attributes = [
+            'global' => [
+                'CLUSTER_AUDIT',
+                'NODES', 'NODES_STATS',
+                'INDICES', 'INDICES_STATS', 'INDICES_CREATE', 'INDICES_REINDEX', 'INDICES_CACHE_CLEAR', 'INDICES_FLUSH', 'INDICES_REFRESH',
+                'SHARDS', 'SHARDS_STATS', 'SHARDS_REROUTE',
+                'MENU_CONFIGURATION',
+                'INDEX_TEMPLATES_LEGACY', 'INDEX_TEMPLATES_LEGACY_CREATE',
+                'REPOSITORIES', 'REPOSITORIES_CREATE',
+                'ELASTICSEARCH_USERS', 'ELASTICSEARCH_USERS_CREATE',
+                'ELASTICSEARCH_ROLES', 'ELASTICSEARCH_ROLES_CREATE',
+                'MENU_TOOLS',
+                'SNAPSHOTS', 'SNAPSHOTS_STATS', 'SNAPSHOTS_CREATE',
+                'CAT', 'CAT_EXPORT',
+                'CONSOLE', 'CONSOLE_POST', 'CONSOLE_PUT', 'CONSOLE_PATCH', 'CONSOLE_DELETE',
+                'MENU_STATS',
+                'MENU_APPLICATION',
+                'APP_USERS', 'APP_USERS_CREATE',
+                'APP_ROLES', 'APP_ROLES_CREATE',
+                'APP_UNINSTALL', 'APP_UPGRADE', 'APP_SUBSCRIPTIONS', 'APP_NOTIFICATIONS',
+            ],
+            'app_user' => [
+                'APP_USER_UPDATE', 'APP_USER_DELETE',
+            ],
+            'app_role' => [
+                'APP_ROLE_UPDATE', 'APP_ROLE_DELETE',
+            ],
+            'index_template_legacy' => [
+                'INDEX_TEMPLATE_LEGACY_UPDATE', 'INDEX_TEMPLATE_LEGACY_DELETE', 'INDEX_TEMPLATE_LEGACY_COPY',
+            ],
+            'index' => [
+                'INDEX_UPDATE',
+                'INDEX_DELETE',
+                'INDEX_CLOSE', 'INDEX_OPEN',
+                'INDEX_CACHE_CLEAR', 'INDEX_FLUSH', 'INDEX_REFRESH',
+                'INDEX_SEARCH', 'INDEX_IMPORT', 'INDEX_EXPORT',
+                'INDEX_ALIASES', 'INDEX_ALIAS_CREATE', 'INDEX_ALIAS_DELETE',
+            ],
+            'node' => [
+                'NODE_SETTINGS', 'NODE_PLUGINS',
+            ],
+            'repository' => [
+                'REPOSITORY_UPDATE', 'REPOSITORY_DELETE', 'REPOSITORY_CLEANUP', 'REPOSITORY_VERIFY',
+            ],
+            'snapshot' => [
+                'SNAPSHOT_DELETE', 'SNAPSHOT_RESTORE', 'SNAPSHOT_FAILURES',
+            ],
+            'elasticsearch_user' => [
+                'ELASTICSEARCH_USER_UPDATE', 'ELASTICSEARCH_USER_DELETE', 'ELASTICSEARCH_USER_ENABLE', 'ELASTICSEARCH_USER_DISABLE',
+            ],
+            'elasticsearch_role' => [
+                'ELASTICSEARCH_ROLE_UPDATE', 'ELASTICSEARCH_ROLE_DELETE', 'ELASTICSEARCH_ROLE_COPY',
+            ],
+        ];
+
+        if (true === $this->callManager->hasFeature('tasks')) {
+            $attributes['global'][] = 'TASKS';
+        }
+
+        if (true === $this->callManager->hasFeature('pipelines')) {
+            $attributes['global'][] = 'PIPELINES';
+            $attributes['global'][] = 'PIPELINES_CREATE';
+            $attributes['pipeline'] = [
+                'PIPELINE_UPDATE', 'PIPELINE_DELETE', 'PIPELINE_COPY',
+            ];
+        }
+
+        if (true === $this->callManager->hasFeature('deprecations')) {
+            $attributes['global'][] = 'DEPRECATIONS';
+        }
+
+        if (true === $this->callManager->hasFeature('license')) {
+            $attributes['global'][] = 'LICENSE';
+        }
+
+        if (true === $this->callManager->hasFeature('license_status')) {
+            $attributes['global'][] = 'LICENSE_START_TRIAL';
+            $attributes['global'][] = 'LICENSE_START_BASIC';
+        }
+
+        if (true === $this->callManager->hasFeature('reload_secure_settings')) {
+            $attributes['global'][] = 'NODES_RELOAD_SECURE_SETTINGS';
+        }
+
+        if (true === $this->callManager->hasFeature('node_usage')) {
+            $attributes['node'][] = 'NODE_USAGE';
+        }
+
+        if (true === $this->callManager->hasFeature('ilm')) {
+            $attributes['global'][] = 'ILM_POLICIES';
+            $attributes['global'][] = 'ILM_POLICIES_STATUS';
+            $attributes['global'][] = 'ILM_POLICIES_CREATE';
+            $attributes['ilm_policy'] = [
+                'ILM_POLICY_UPDATE', 'ILM_POLICY_DELETE', 'ILM_POLICY_COPY', 'ILM_POLICY_APPLY',
+            ];
+            $attributes['index'][] = 'INDEX_LIFECYCLE';
+        }
+
+        if (true === $this->callManager->hasFeature('enrich')) {
+            $attributes['global'][] = 'ENRICH_POLICIES';
+            $attributes['global'][] = 'ENRICH_POLICIES_STATS';
+            $attributes['global'][] = 'ENRICH_POLICIES_CREATE';
+            $attributes['enrich_policy'] = [
+                'ENRICH_POLICY_DELETE', 'ENRICH_POLICY_COPY', 'ENRICH_POLICY_EXECUTE',
+            ];
+        }
+
+        if (true === $this->callManager->hasFeature('slm')) {
+            $attributes['global'][] = 'SLM_POLICIES';
+            $attributes['global'][] = 'SLM_POLICIES_STATS';
+            $attributes['global'][] = 'SLM_POLICIES_STATUS';
+            $attributes['global'][] = 'SLM_POLICIES_CREATE';
+            $attributes['slm_policy'] = [
+                'SLM_POLICY_UPDATE', 'SLM_POLICY_DELETE', 'SLM_POLICY_COPY', 'SLM_POLICY_EXECUTE',
+            ];
+        }
+
+        if (true === $this->callManager->hasFeature('composable_template')) {
+            $attributes['global'][] = 'COMPONENT_TEMPLATES';
+            $attributes['global'][] = 'COMPONENT_TEMPLATES_CREATE';
+            $attributes['component_template'] = [
+                'COMPONENT_TEMPLATE_UPDATE', 'COMPONENT_TEMPLATE_DELETE', 'COMPONENT_TEMPLATE_COPY',
+            ];
+
+            $attributes['global'][] = 'INDEX_TEMPLATES';
+            $attributes['global'][] = 'INDEX_TEMPLATES_CREATE';
+            $attributes['index_template'] = [
+                'INDEX_TEMPLATE_UPDATE', 'INDEX_TEMPLATE_DELETE', 'INDEX_TEMPLATE_COPY',
+            ];
+        }
+
+        if (true === $this->callManager->hasFeature('dangling_indices')) {
+            $attributes['global'][] = 'DANGLING_INDICES';
+            $attributes['global'][] = 'DANGLING_INDICES_IMPORT';
+            $attributes['global'][] = 'DANGLING_INDICES_DELETE';
+        }
+
+        if (true === $this->callManager->hasFeature('data_streams')) {
+            $attributes['global'][] = 'DATA_STREAMS';
+            $attributes['global'][] = 'DATA_STREAMS_CREATE';
+            $attributes['data_stream'] = [
+                'DATA_STREAM_STATS', 'DATA_STREAM_DELETE',
+            ];
+        }
+
+        if (true === $this->callManager->hasFeature('sql')) {
+            $attributes['global'][] = 'SQL';
+        }
+
+        if (true === $this->callManager->hasFeature('tombstones')) {
+            $attributes['global'][] = 'INDEX_GRAVEYARD';
+        }
+
+        if (true === $this->callManager->hasFeature('clone_snapshot')) {
+            $attributes['snapshot'][] = 'SNAPSHOT_CLONE';
+        }
+
+        if (true === $this->callManager->hasFeature('cluster_settings')) {
+            $attributes['global'][] = 'CLUSTER_SETTINGS';
+            $attributes['global'][] = 'CLUSTER_SETTING_EDIT';
+            $attributes['global'][] = 'CLUSTER_SETTING_REMOVE';
+            $attributes['global'][] = 'CLUSTER_DISK_THRESHOLDS';
+        }
+
+        if (true === $this->callManager->hasFeature('allocation_explain')) {
+            $attributes['global'][] = 'CLUSTER_ALLOCATION_EXPLAIN';
+        }
+
+        if (true === $this->callManager->hasFeature('freeze_unfreeze')) {
+            $attributes['index'][] = 'INDEX_FREEZE';
+            $attributes['index'][] = 'INDEX_UNFREEZE';
+        }
+
+        if (true === $this->callManager->hasFeature('force_merge')) {
+            $attributes['global'][] = 'INDICES_FORCE_MERGE';
+            $attributes['index'][] = 'INDEX_FORCE_MERGE';
+        }
+
+        if (true === $this->callManager->hasFeature('delete_by_query')) {
+            $attributes['index'][] = 'INDEX_EMPTY';
+        }
+
+        if (true === $this->callManager->hasFeature('remote_clusters')) {
+            $attributes['global'][] = 'REMOTE_CLUSTERS';
+        }
+
+        return $attributes;
     }
 }
