3a1191cd887450459243a19842efb2b6a8ceba58
Stéphane Dion <divers@sdion.net>
2021-11-20 20:31:50 +0100

    -

--- phpstan-constants.php
+++ phpstan-constants.php
@@ -1,17 +1,5 @@
 <?php
-define('GENERATED_NAME', 'phpunit-'.uniqid());
-define('GENERATED_NAME_SYSTEM', '.phpunit-'.uniqid());
-define('GENERATED_NAME_UPPER', getRandomString(8));
-define('GENERATED_EMAIL', 'phpunit-'.uniqid().'@test.com');
-
-function getRandomString($length = 8)
-{
-    $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $string = '';
-
-    for ($i = 0; $i < $length; $i++) {
-        $string .= $characters[mt_rand(0, strlen($characters) - 1)];
-    }
-
-    return $string;
-}
+define('GENERATED_NAME', 'phpstan');
+define('GENERATED_NAME_SYSTEM', 'phpstan');
+define('GENERATED_NAME_UPPER', 'phpstan');
+define('GENERATED_EMAIL', 'phpstan');
