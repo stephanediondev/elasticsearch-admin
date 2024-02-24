0a415ee76724da3433783f17bae2071394a04ae7
Stéphane Dion <divers@sdion.net>
2024-02-24 16:41:06 +0100

    Migrating from Webpack Encore to AssetMapper

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import './styles/app.css';
+
+import { Dropdown, Modal, Toast } from 'bootstrap';
+
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle');
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new Dropdown(dropdownToggleEl));
+
 (() => {
     'use strict'
 
@@ -53,23 +62,17 @@
     })
 })();
 
-require('jquery');
-global.$ = global.jQuery = $;
+import $ from 'jquery';
 
-require('bootstrap');
+import saveAs from 'file-saver';
 
-import { saveAs } from 'file-saver';
-var slug = require('slug');
+import slug from 'slug';
+
 slug.charmap['/'] = '-';
 slug.charmap['?'] = '-';
 slug.charmap['='] = '-';
-global.slug = slug;
 
-global.sleep = function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-global.createToast = function createToast(body) {
+var createToast = function createToast(body) {
     var toast = `<div class="toast bg-dark text-light border border-secondary" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-body">
             ${body}
@@ -108,12 +111,12 @@ if (buttonInstall) {
     });
 }
 
-global.serviceWorkerEnabled = false;
+var serviceWorkerEnabled = false;
 
 if('serviceWorker' in navigator && 'https:' == window.location.protocol) {
     navigator.serviceWorker.register(app_base_url + 'serviceworker.js')
     .then(function(ServiceWorkerRegistration) {
-        global.serviceWorkerEnabled = true;
+        serviceWorkerEnabled = true;
 
         if (buttonInstall) {
             var standalone = window.matchMedia('(display-mode: standalone)');
